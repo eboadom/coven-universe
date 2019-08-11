db97de035e00aa7677d877ae335f5c3da25b4800
Andrew Kim <akinhwan@gmail.com>
2019-08-11 19:45:39 -0400

    icons, add vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,24 @@
 <template>
+  <!-- <v-app> -->
+  <!-- <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+  </v-app-bar>-->
+
+  <!-- <v-content> -->
+  <!-- <Home /> -->
+  <!-- </v-content> -->
+  <!-- </v-app> -->
   <div id="app">
     <div id="nav">
       <router-link to="/">Landing</router-link>|
@@ -9,23 +29,29 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  })
+};
+</script>
 <style>
 @font-face {
   font-family: "exocet";
   src: url("./assets/exocet.ttf");
 }
-
 @font-face {
   font-family: "codesaver";
   src: url("./assets/codesaver.otf");
 }
-
 body {
   margin: 0;
   overflow: hidden;
   background: transparent url("./assets/background.svg") no-repeat fixed center;
 }
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -35,3 +61,4 @@ body {
   margin: 0; */
 }
 </style>
+
