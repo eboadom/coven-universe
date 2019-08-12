7c4d60829d8277f84a1b9b81b4102c4a5c7a04e4
eboado <ernesto.boado.moreira@gmail.com>
2019-08-12 19:38:37 +0000

    - Continue implementation of dao-graph.

---
+++ contracts/contracts/GenericScheme.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.4;
+
+import {GenericScheme as GenericSchemeArc} from "@daostack/arc/contracts/universalSchemes/GenericScheme.sol";
+
+contract GenericScheme is GenericSchemeArc {}
