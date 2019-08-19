e38a4ee58ca6eff8de7bd60ef8e6229bd1cb4387
eboado <ernesto.boado.moreira@gmail.com>
2019-08-19 18:02:03 +0000

    - Added Dapper util detection on Landing.

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="landing">
-    <img class="left-cloud" alt="Left Cloud" src="../assets/cloud-left.svg" />
-    <img class="right-cloud" alt="Right Cloud" src="../assets/cloud-right.svg" />
+    <img class="left-cloud" alt="Left Cloud" src="../assets/cloud-left.svg">
+    <img class="right-cloud" alt="Right Cloud" src="../assets/cloud-right.svg">
     <!-- <img class="bottom-cheese" alt="Bottom Cheese" src="../assets/bottom-cheese.svg"> -->
-    <img class="bottom-cloud" alt="Bottom Cloud" src="../assets/bottom-cloud.svg" />
+    <img class="bottom-cloud" alt="Bottom Cloud" src="../assets/bottom-cloud.svg">
 
     <!-- <img class="paper-banner" alt="Paper Banner" src="../assets/paper-banner.svg"> -->
     <div class="container">
       <!-- <h1>Cheeze DAO</h1> -->
-      <img src="../assets/cheezedao.svg" alt />
+      <img src="../assets/cheezedao.svg" alt>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <button class="button">
         <router-link to="/home">Enter the Cheeze</router-link>
@@ -18,8 +18,12 @@
 </template>
 
 <script>
+import { initDapper } from "../utils/dapper.js";
 export default {
   name: "landing",
+  beforeCreate() {
+    initDapper();
+  },
   components: {}
 };
 </script>
