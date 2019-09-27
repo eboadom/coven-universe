1834e69d608d0f772cb510a7fc815358f299bf3d
andyk <kyzia.ru@gmail.com>
2019-09-27 10:51:28 +0300

    add no wizards text

--- client/src/views/MyWizards.vue
+++ client/src/views/MyWizards.vue
@@ -9,7 +9,7 @@
           <button v-if="formattedWizards.length" class="button button-small" @click.prevent="mint" >Mint Wizard</button>
         </div>
         <div class="no-wizards" v-if="!formattedWizards.length">
-          texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts texts
+          It seems you don’t have any Wizards at this sad point in your life. Luckily for you, you can either connect to your wallet and hopefully find a Wizard there, or just mint one.
           <button class="button button-small" @click.prevent="mint" >Mint Wizard</button>
         </div>
         <v-data-table
